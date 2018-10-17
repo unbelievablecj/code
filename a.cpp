8ab7fd1a0b6bd3183957617d2025368196100ebc
unbelievablecj <unbelievablecj@outlook.com>
2018-10-17 15:35:48 +0800

    aa

--- a.cpp
+++ a.cpp
@@ -1,24 +1,25 @@
 #include<stdio.h>
 #include<iostream>
+#include<stack>
+#include<string>
 using namespace std;
 int main(){
     //freopen("1.txt","r",stdin);
-    int t;
-    scanf("%d",&t);
     stack<int> sta;
     string s;
     cin>>s;
-    for(int i=0;i<s.length;i++){
+    for(int i=0;i<s.length();i++){
         if(s[i]=='-'){
-            if(!sta.empty()&&sta.top=='-1'){
+            if(!sta.empty()&&sta.top()==-1){
                 sta.pop();
             }
             else{
                 sta.push(-1);
             }
+            cout<<sta.size()<<endl;
         }
         else{
-            if(!sta.empty()&&sta.top=='1'){
+            if(!sta.empty()&&sta.top()==1){
                 sta.pop();
             }
             else{
