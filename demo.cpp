2a8dbfc62f78292bfe76c394d44870062a75ca7e
unbelievablecj <unbelievablecj@outlook.com>
2018-10-19 00:39:15 +0800

    add demo

---
+++ demo.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <cstdio>
+#include <string>
+#include <cmath>
+#include <queue>
+#include <set>
+#include <map>
+#include <stack>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <complex>
+using namespace std;
+typedef long long ll;
+typedef long double db;
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+#define de(x) cout << #x << "=" << x << endl
+#define dd(x) cout << #x << "0" << x << ", "
+#define rep(i,a,b) for(int i=a;i<(b);++i)
+#define repb(i,a,b) for(int i=a;i<=(b);i++)
+#define all(x) (x).begin(),(x).end()
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+const int N = 101010;
+int main(){
+	
+	return 0;
+} 
